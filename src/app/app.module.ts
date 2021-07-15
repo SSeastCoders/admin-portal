82b69fb258134cc18774e096a8fd2cd0a179b025
htbak <hazel.baker-harvey@smoothstack>
2021-07-15 18:21:34 -0400

    ECB 151 - added login, layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 // app/app.module.ts
 import { NgModule } from  '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
+
 import { MaterialModule } from  './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from  '@angular/material/toolbar';
@@ -15,8 +15,6 @@ import { MatToolbarModule } from  '@angular/material/toolbar';
  
   ],
   declarations: [
-    AppModule,
-    // ...
   ]
 })
 
