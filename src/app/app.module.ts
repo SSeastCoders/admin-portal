1e6c877e079b68bb14ff03f945e10bb49c2e9fc9
htbak <hazel.baker-harvey@smoothstack>
2021-07-15 17:46:18 -0400

    ECB -151 making top bar on login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+// app/app.module.ts
+import { MyMaterialModule } from  './material.module';
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MyMaterialModule,
+ 
   ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
