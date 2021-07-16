1967c0d0c994f8287056ffcf88652d6544da8412
htbak <hazel.baker-harvey@smoothstack>
2021-07-15 21:01:10 -0400

    ECB 151 - adding header footer attempt INCOMPLETE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
-// app/app.module.ts
-import { NgModule } from  '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MaterialModule } from  './material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from  '@angular/material/toolbar';
- 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    MatToolbarModule
- 
+    AppRoutingModule
   ],
-  declarations: [
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-
-export  class  AppModule { }
+export class AppModule { }
