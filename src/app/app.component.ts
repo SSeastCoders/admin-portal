c788eb1ef5e221efb62a18c78c10aaa57a734bf2
Amanda R <amanda.rolon@smoothstack.com>
2021-07-22 15:17:41 -0400

    pagination working with fake data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from './services/employee.service';
 import { Employee } from './models/employee';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,26 @@ import { Employee } from './models/employee';
 })
 export class AppComponent implements OnInit {
   public employees: Employee[];
+  public fakers = [
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+    { faker: 'faker17' },
+  ];
+  public pageSlice = this.fakers.slice(0, 10);
 
   constructor(private employeeService: EmployeeService) {
     this.employees = [];
@@ -28,5 +49,15 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  OnPageChange(event: PageEvent) {
+    console.log(event);
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.fakers.length) {
+      endIndex = this.fakers.length;
+    }
+    this.pageSlice = this.fakers.slice(startIndex, endIndex);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeService } from './services/employee.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [AppComponent, EmployeesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatPaginatorModule,
+  ],
   providers: [EmployeeService],
   bootstrap: [AppComponent],
 })
