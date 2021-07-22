32226fb315c605832537995db88c521292c5b7c9
htbak <hazel.baker-harvey@smoothstack>
2021-07-22 11:15:57 -0400

    ECB - 151 env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:8443',
+  apiUrl: 'https://localhost:8443',
   //apiUrl: 'https://localhost:8443',
   production: false,
   //NODE_TLS_REJECT_UNAUTHORIZED=0
