7f5e7906e823b26a79012e3304b1508e1b831d88
htbak <hazel.baker-harvey@smoothstack>
2021-07-22 22:51:14 -0400

    ECB151 - rerouting with guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 import { LoginComponent } from './login/login.component';
 import { UserFormComponent } from './observables/user-list/user-form/user-form.component';
 import { UserListComponent } from './observables/user-list/user-list.component';
+import { AuthGuard } from './services/guard/guard.guard';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'users', component: UserListComponent}
+  {path: 'users', component: UserListComponent},
+  { canActivate: [AuthGuard], path: 'cookies', component: UserListComponent }
   //{path: 'users', component: UserFormComponent}
 ];
 
