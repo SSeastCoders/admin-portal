233dd9d8fa2462f006d1684a26eb8257317eeccc
htbak <hazel.baker-harvey@smoothstack>
2021-08-03 23:25:49 -0400

    ECB-158 cont server error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -43,7 +43,7 @@ export class LoginComponent implements OnInit{
         .login(this.credentials)
         .subscribe(
           (res) => {
-            console.log('success')},
+          },//console.log('success')},
           (error : HttpErrorResponse) => {
             this.serverError = true;
           }
