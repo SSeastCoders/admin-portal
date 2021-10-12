0fa216b2ad16173d3cc77dcee38005382c05f808
Amanda R <amanda@Amandas-MacBook-Pro.local>
2021-10-12 11:43:55 -0400

    fhsjkhfjdsk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,5 @@ export const environment = {
   userUrl: 'https://localhost:8443',
   accountUrl: 'http://localhost:8223/api/v1',
   apiUrl: 'https://localhost:8443',
+  cardUrl: 'http://localhost:8225',
 };
